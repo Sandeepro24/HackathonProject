14a4d295e473de5ee4851e3a39e6126dca62a288
sandeep <sandeep>
2024-02-29 00:28:32 +0530

    updating pom

--- src/test/java/com/LoanEMI/StepRunner/TestRunner.java
+++ src/test/java/com/LoanEMI/StepRunner/TestRunner.java
@@ -4,10 +4,10 @@ import org.junit.runner.RunWith;
 import org.testng.annotations.Test;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
 
 		features = { "src/test/resources/Features/EMIcalculatorEndToEnd.feature",}, glue = "com.LoanEMI.StepDefinitions", plugin = {"pretty", "rerun:target/rerun.txt","html:Cucumber-reports/report.html",
